8223f0332dd5319d5cc8fe19230d4b7802568c92
sameer <sameer@git.com>
2025-02-22 10:47:11 +0530

    basic pokedex setup done

---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import axios from 'axios' ;
+import './PokemonList.css'
+import Pokemon from "../Pokemon/Pokemon";
+
+
+function PokemonList(){
+    const [isloading,setisloading]=useState(true);
+    const [pokemonlist,setpokemonlist] =useState([]);
+    
+async function downloadPokemons(){
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    const pokemonsresults = response.data.results;
+    const pokemonpromises = pokemonsresults.map((pokemon)=> axios.get(pokemon.url));
+    const pokemonData = await axios.all(pokemonpromises);
+    console.log(pokemonData);
+    const res = pokemonData.map((pokeData)=>{
+       const pokemon = pokeData.data;
+       return{
+        id : pokemon.id,
+        name : pokemon.name,
+        image: (pokemon.sprites.other)? pokemon.sprites.other.dream_world.front_default :pokemon.sprites.front_shiny,
+        types: pokemon.types
+           }
+    });
+    console.log(res);
+    setpokemonlist(res);
+    setisloading(false)
+}
+              useEffect(()=>{
+                downloadPokemons();
+               } ,[])
+    return (
+        <div className="pokemon-list-wrapper">
+            <div>  List of pokemons</div>
+          
+            {(isloading)?"Loading" : 
+            pokemonlist.map((pokemon)=> <Pokemon name={pokemon.name} image ={pokemon.image} key={pokemon.id}/>)
+            }
+        </div>
+    )
+}
+export default PokemonList
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,9 @@
+import './Search.css'
+function Search(){
+    return(
+        <div className="search-wrapper">
+        <input id="pokemon-name-search" type="text" placeholder="enter pokemon...."/>
+        </div>
+    );
+}
+export default Search
